cefb5e9b9c527798159264e36e45f83b5b1308a9
Michael Heer <mheer91@icloud.com>
2021-11-12 17:47:28 -0500

    trying to fix gh pages

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -8,12 +8,12 @@ export default function NavHeader({ currentPage, pageChange }) {
             <Container fluid>
                 <Navbar.Brand href="#aboutme" onClick={() => pageChange('Aboutme')}>Michael Heer</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
+                <Navbar.Collapse id="responsive-navbar-nav" >
                     <Nav variant="pills" defaultActiveKey="/aboutme" className="justify-contents-end secondary">
-                        <Nav.Link href="#aboutme" onClick={() => pageChange('Aboutme')}>About Me</Nav.Link>
-                        <Nav.Link href="#portfolio" onClick={() => pageChange('Projects')}>Portfolio</Nav.Link>
-                        <Nav.Link href="#contactme" onClick={() => pageChange('ContactMe')}>Contact Me</Nav.Link>
-                        <Nav.Link href="#resume" onClick={() => pageChange('Resume')}>Resume</Nav.Link>
+                        <Nav.Link href="/React-Portfolio/#aboutme" onClick={() => pageChange('Aboutme')}>About Me</Nav.Link>
+                        <Nav.Link href="/React-Portfolio/#portfolio" onClick={() => pageChange('Projects')}>Portfolio</Nav.Link>
+                        <Nav.Link href="/React-Portfolio/#contactme" onClick={() => pageChange('ContactMe')}>Contact Me</Nav.Link>
+                        <Nav.Link href="/React-Portfolio/#resume" onClick={() => pageChange('Resume')}>Resume</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
